65f6f8bb85f4e50c8fa146aa1e3947538af58f01
bduyphuong12 <65779951+bduyphuong12@users.noreply.github.com>
2022-10-13 22:13:56 +0700

    giao dien code

---
+++ reactjs/src/Components/PageLayout/Header/navbar.jsx
@@ -0,0 +1,60 @@
+// import { useState } from "react";
+
+import { Link, useHistory } from 'react-router-dom';
+import './navbar.css';
+import { useEffect, useState } from 'react';
+import logo from '../../../assets/logo/logo1.png';
+function NavBar() {
+  return (
+    <nav className='navbar navbar-expand navbar-light '>
+      <div className='nav-left-side'>
+        <Link to={'/'}>
+          <img className='navbar-logo' src={logo} alt='logo' />
+        </Link>
+      </div>
+
+      <div className='nav-right-side'>
+        {/* <>
+          <div className='name-user'>Hi,</div>
+          <div>
+            {/* <img src="https://i.ibb.co/PCjW83Y/avt.png" className="avt" /> */}
+        {/* <div className='dropdown'>
+              <i className=' fa fa-solid fa-caret-down dropbtn'></i>
+              <div className='drop-down-item dropdown-content'>
+                <Link to={'/setting'}>Tài Khoản</Link>
+                <button>Logout</button>
+              </div>
+            </div>
+          </div>
+        </> */}{' '}
+        <Link to={'/login'}>
+          <img src='https://i.ibb.co/PCjW83Y/avt.png' className='avt' />
+          <button className='navbar-btn' id='btnSignUp'>
+            LOGIN
+          </button>
+        </Link>
+      </div>
+    </nav>
+    // <>
+    //   <div className='header'>
+    //     <div className='header-logo'>
+    //       <Link to={'/'}>
+    //         <img className='navbar-logo' src={logo} alt='logo' />
+    //       </Link>
+    //     </div>
+    //   </div>
+    //   <div className='nav-right'>
+    //     <Link to={'/login'}>
+    //       <div>
+    //         <img src='https://i.ibb.co/PCjW83Y/avt.png' className='avt' />
+    //         <button className='navbar-btn' id='btnSignUp'>
+    //           LOGIN
+    //         </button>
+    //       </div>
+    //     </Link>
+    //   </div>
+    // </>
+  );
+}
+
+export default NavBar;
--- reactjs/src/modules/home/index.jsx
+++ reactjs/src/modules/home/index.jsx
@@ -1,14 +1,31 @@
 import React, { useEffect } from 'react';
-
+import './index.css';
 import useGlobalStore from '../../hook/useGlobal';
-
+import image from '../../assets/logo/img.png';
 const HomeScreen = () => {
-  const { test, check } = useGlobalStore();
-  useEffect(() => {
-    test('thu cu nha');
-  });
-  console.log('check str', check);
-  return <div>Home page ccasdasdasd thai moi code</div>;
+  var showAva = function (event) {
+    var avaEdit = document.getElementById('output');
+    try {
+      avaEdit.src = URL.createObjectURL(event.target.files[0]);
+    } catch (error) {}
+  };
+  return (
+    <div className='homepage'>
+      <h1>CONVERT IMAGE TO TEXT</h1>
+      <div className='body-img'>
+        <div className='upload-image'>
+          <form runat='server' className='upload'>
+            <img src={image} alt='' id='output' className='img' />
+            {/* <img className='avatar-img' src='' alt='' id='output' width='200px' height='200px' /> */}
+            <input accept='image/*' className='inputImg' type='file' onChange={showAva} />
+          </form>
+        </div>
+      </div>
+      <a class='button1' href='*'>
+        <span class='btn1'>Convert</span>
+      </a>
+    </div>
+  );
 };
 
 export default HomeScreen;
