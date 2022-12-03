869f90728cc4c4f6a7e87832002dbf1470ac6fee
Phan Hữu Minh Trí <triphm@bap.jp>
2022-12-03 14:37:13 +0700

    code Fe

--- reactjs/src/modules/home/index.jsx
+++ reactjs/src/modules/home/index.jsx
@@ -2,15 +2,16 @@ import React, { useRef, useState } from 'react';
 import './index.css';
 import { Link } from 'react-router-dom';
 import '../../assets/bootstrap/bootstrap.min.css';
-// import CanvasDraw from 'react-canvas-draw';
+import CanvasDraw from 'react-canvas-draw';
 import logo2 from '../../assets/logo/logo2.png';
 import pencil from '../../assets/logo/pencil.png';
 import searchI from '../../assets/logo/search.png';
 import triangle from '../../assets/logo/obj_triangle.png';
 function HomeScreen() {
   const canvasRef = useRef(null);
-  const canvas = '#FCA5A5';
-  const brush = 4;
+  const [canvas, setBrush] = useState('#FCA5A5');
+  const [brush, setThick] = useState(50);
+
   const [isShown, setIsShown] = useState(false);
 
   const handleClick = (event) => {
@@ -37,6 +38,7 @@ function HomeScreen() {
                   <img src={searchI} alt='' className='searchI' />
                 </Link>
               </div>
+              <div className='draw-writing'>
               {isShown && (
                 <div className='hand-writing-area-wrapper'>
                   <div className='draw-kanji-top-bar'>
@@ -68,18 +70,51 @@ function HomeScreen() {
                     </div>
                     <div className='hand-writing-area margin-btm'>
                       <div className='canvas-draw-kanji'>
-                        {/* <CanvasDraw
-                    className='canvas-box'
-                    ref={canvasRef}
-                    brushColor={canvas}
-                    brushRadius={brush}
-                    hideGrid={true}
-                  /> */}
+                          <CanvasDraw ref={canvasRef} brushColor={canvas} brushRadius={brush} hideGrid={true} />
+                          <button
+                            onClick={() => {
+                              canvasRef.current.undo();
+                            }}
+                          >
+                            UNDO
+                          </button>
+                          <button
+                            onClick={() => {
+                              canvasRef.current.clear();
+                            }}
+                          >
+                            CLEAR
+                          </button>
+                          <br />
+                          <label>Colour picker</label>
+                          <input
+                            style={{ background: { canvas } }}
+                            type='color'
+                            value={canvas}
+                            onChange={(event) => {
+                              console.log(event.target.value);
+                              setBrush(event.target.value);
+                            }}
+                          />
+
+                          <br />
+                          <label>Brush Thickness</label>
+                          <div className='thickness'></div>
+                          <input
+                            min='2'
+                            max='50'
+                            type='range'
+                            onChange={(event) => {
+                              console.log(event.target.value);
+                              setThick(event.target.value);
+                            }}
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
               )}
+              </div>
             </div>
           </div>
         </div>
